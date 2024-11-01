d68d0e3ccc2c99e2cf04755176d8ad91cd90a67d
SophiaRauls <sophiarauls@gmail.com>
2024-11-01 23:01:21 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
-for (let index = 0; index < 10; index++) {
+let Temp = 0
+for (let index = 0; index < 2; index++) {
     pins.servoWritePin(AnalogPin.P1, 45)
     pins.servoWritePin(AnalogPin.P2, 45)
-    basic.pause(10000)
+    basic.pause(1000)
     pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 180)
-    basic.pause(10000)
+    basic.pause(1000)
+}
+while (true) {
+    Temp = pins.analogReadPin(AnalogReadWritePin.C16)
+    basic.showNumber(Temp)
+    if (Temp >= 495) {
+        pins.servoWritePin(AnalogPin.P1, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P1, 45)
+    }
 }
